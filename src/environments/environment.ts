d1050e4edb5d155721f1dbb00923ace10d2d7cbf
Reenert23 <henningrene1@gmail.com>
2026-07-26 15:23:18 +0200

    Point development API to Render backend

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: 'http://localhost:8080/api' // dein lokales Spring Boot Backend
+  apiUrl: 'https://zipfel-backend.onrender.com/api' // Render Backend
 };
 
